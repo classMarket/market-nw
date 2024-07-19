abe27b2764a7d59059d6eee52e0e3c29b1e784ab
jo <whwo1214@gmail.com>
2024-07-19 11:24:10 +0900

    로그인 미세 수정

--- src/main/java/market_nw/market_nw/controller/LoginController.java
+++ src/main/java/market_nw/market_nw/controller/LoginController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import market_nw.market_nw.dto.LoginDto;
 import market_nw.market_nw.security.JwtTokenProvider;
 import market_nw.market_nw.service.UserService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,7 +24,6 @@ public class LoginController {
     private final UserService userService;
     private final JwtTokenProvider jwtTokenProvider;
     
-//Binding Result 는 서버 사이드 렌더링 에서 쓰이기에 없앴습니다.
     @PostMapping("/login")
     public ResponseEntity<?> login(@Valid @RequestBody LoginDto loginDto) {
         String token = userService.normal_login(loginDto); //로그인 성공시 토큰으로 변환
@@ -33,8 +33,13 @@ public class LoginController {
             response.put("message", "이메일 또는 비밀번호가 맞지 않습니다.");
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
         }
-        // 로그인 성공시 토큰 으로 반환
-        return ResponseEntity.ok(token);
+        // 로그인 성공시 토큰을 헤더에 추가하여 반환
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body("JWT token 로그인 성공");
     }
 
     @GetMapping("/test")
